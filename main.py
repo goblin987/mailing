284a7a4562001e2d9d216bc8760a2a8926753c1a
goblin987 <goblingoblinu@gmail.com>
2025-05-07 13:33:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -123,29 +123,6 @@ def main():
     # --- Register Handlers ---
     log.info("Registering handlers...")
     if handlers.main_conversation:
-        # Set up the conversation handler with async support
-        handlers.main_conversation.entry_points = [
-            CommandHandler('start', handlers.start_command, filters=Filters.chat_type.private, run_async=True),
-            CommandHandler('admin', handlers.admin_command, filters=Filters.chat_type.private, run_async=True),
-            CommandHandler('cancel', handlers.cancel_command, filters=Filters.chat_type.private, run_async=True),
-            CallbackQueryHandler(handlers.main_callback_handler, run_async=True)
-        ]
-        
-        # Set up fallback handlers
-        handlers.main_conversation.fallbacks = [
-            CommandHandler('cancel', handlers.cancel_command, filters=Filters.chat_type.private, run_async=True),
-            CommandHandler('start', handlers.start_command, filters=Filters.chat_type.private, run_async=True),
-            CommandHandler('admin', handlers.admin_command, filters=Filters.chat_type.private & Filters.user(ADMIN_IDS), run_async=True),
-            CallbackQueryHandler(handlers.main_callback_handler, run_async=True),
-            MessageHandler(Filters.text & ~Filters.command & Filters.chat_type.private, handlers.conversation_fallback, run_async=True)
-        ]
-        
-        # Update all state handlers to use run_async=True
-        for state in handlers.main_conversation.states:
-            for handler in handlers.main_conversation.states[state]:
-                if isinstance(handler, (CommandHandler, MessageHandler, CallbackQueryHandler)):
-                    handler.run_async = True
-        
         dp.add_handler(handlers.main_conversation)
         log.info("Main conversation handler registered.")
     else:
